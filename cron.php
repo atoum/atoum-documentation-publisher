21ac2ccd3c2121c1455d80d7c6bfa7058e795fb1
marmotz <rlittolff@gmail.com>
2013-02-02 14:38:55 +0100

    fix PATH

--- cron.php
+++ cron.php
@@ -4,19 +4,14 @@
     define('SIA_PATH',          __DIR__ . '/sia');
     define('DOC_SOURCE',        __DIR__ . '/sources');
     define('DOC_OUTPUT',        $argv[1]);
-
-    if(!defined('PHP_BINARY')) {
-        define('PHP_BINARY', '/usr/local/bin/php');
-    }
-
-    define('GIT_BINARY',  '/usr/local/bin/git');
-    define('CURL_BINARY', '/usr/local/bin/curl');
+    define('PATH',              '$PATH:/bin:/usr/bin:/usr/local/bin');
 
     function writeln($txt = '') {
         echo "$txt\n";
     }
 
     function command($command) {
+        $command = PATH . '; ' . $command;
         writeln("$ $command");
         passthru($command);
         writeln();
@@ -29,27 +24,27 @@
     ) {
         // get composer
         if(!file_exists(COMPOSER_PATH)) {
-            command(CURL_BINARY . ' -s http://getcomposer.org/installer | ' . PHP_BINARY);
+            command('curl -s http://getcomposer.org/installer | php');
         }
         else {
-            command(PHP_BINARY . ' composer.phar self-update');
+            command('php composer.phar self-update');
         }
 
         // get sia
         if(!file_exists(SIA_PATH)) {
-            command(GIT_BINARY . ' clone https://github.com/marmotz/sia.git');
+            command('git clone https://github.com/marmotz/sia.git');
             chdir(SIA_PATH);
-            command(PHP_BINARY . ' ../composer.phar install');
+            command('php ../composer.phar install');
         }
         else {
             chdir(SIA_PATH);
-            command(GIT_BINARY . ' pull');
-            command(PHP_BINARY . ' ../composer.phar update');
+            command('git pull');
+            command('php ../composer.phar update');
         }
 
         // get documentation source
         command('rm -rf ' . DOC_SOURCE);
-        command(GIT_BINARY . " clone $repoUrl " . DOC_SOURCE);
+        command("git clone $repoUrl " . DOC_SOURCE);
 
         // create ouput directory
         if(!file_exists(DOC_OUTPUT)) {
@@ -66,8 +61,8 @@
 
         // generate documentation
         chdir(SIA_PATH);
-        command(PHP_BINARY . ' bin/sia -i ' . DOC_SOURCE . '/fr/Contents/ -o ' . DOC_OUTPUT . '/fr -t atoum');
-        command(PHP_BINARY . ' bin/sia -i ' . DOC_SOURCE . '/en/Contents/ -o ' . DOC_OUTPUT . '/en -t atoum');
+        command('php bin/sia -i ' . DOC_SOURCE . '/fr/Contents/ -o ' . DOC_OUTPUT . '/fr -t atoum');
+        command('php bin/sia -i ' . DOC_SOURCE . '/en/Contents/ -o ' . DOC_OUTPUT . '/en -t atoum');
 
         // reinit flag file
         file_put_contents(FLAG_FILE, '');
